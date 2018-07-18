e06ad5ddf3e96ad28071431780077c4ceb458595
HappyJeannie <436030684@qq.com>
2018-07-18 08:34:20 +0800

    引入 jquery

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ function component () {
   var $element = $('<div></div>');
 
   /* lodash is required for the next line to work */
-  // element.innerHTML = _.join(['Hello','webpack'], ' ');
+  $element.html(_.join(['Hello','webpack'], ' '));
 
   
-  return element;
+  return $element;
 }
 
-
-document.body.appendChild(component());
+$('body').append(component());
