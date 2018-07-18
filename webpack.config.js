1de81b58ba667e984cff9fc42082dbc5d91c53ba
HappyJeannie <436030684@qq.com>
2018-07-18 09:51:04 +0800

    使用 webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,11 @@ const webpack = require('webpack');
 
 module.exports = {
   entry : './src/index.js',
-  // optimization: {
-  //   minimizer: [
-  //     new UglifyJsPlugin({
-  //       cache: true,
-  //       parallel: true,
-  //       sourceMap: true // set to true if you want JS source maps
-  //     })
-  //   ]
-  // },
+  devtool : 'inline-source-map',
+  devServer : {
+    contentBase : './dist'
+  },
   plugins : [
-    
-    // new UglifyJsPlugin({
-    //   test: /\.js($|\?)/i,
-    //   uglifyOptions:{
-
-    //   }
-    // })
-    // new webpack.optimize.minimize({
-    //   compress: {
-    //       warnings: false
-    //    }
-    // })
   ],
   output : {
     filename: 'bundle.js',
