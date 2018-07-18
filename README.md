8fa267a8ce9fb620f2984c955a52a549431cc0ed
HappyJeannie <436030684@qq.com>
2018-07-18 09:29:46 +0800

    压缩js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # todo-list
-react
+### 一、代码压缩
+在 package.json 的脚本指令中，只要加上环境变量，系统会针对生产环境的代码进行压缩，如 'build' 指令对应 `webpack --mode=production` ，那么打包出来的代码就是经过压缩后的。以上情况至针对 webpack 4.x
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require('path');
+//const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const webpack = require('webpack');
 
 module.exports = {
   entry : './src/index.js',
+  // optimization: {
+  //   minimizer: [
+  //     new UglifyJsPlugin({
+  //       cache: true,
+  //       parallel: true,
+  //       sourceMap: true // set to true if you want JS source maps
+  //     })
+  //   ]
+  // },
+  plugins : [
+    
+    // new UglifyJsPlugin({
+    //   test: /\.js($|\?)/i,
+    //   uglifyOptions:{
+
+    //   }
+    // })
+    // new webpack.optimize.minimize({
+    //   compress: {
+    //       warnings: false
+    //    }
+    // })
+  ],
   output : {
     filename: 'bundle.js',
     path : path.resolve(__dirname,'dist')
   }
+  
 }
